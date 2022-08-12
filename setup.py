d067df25e0b85ad86e4711ac561229232c086a9a
Martin Schätz <61615369+martinschatz-cz@users.noreply.github.com>
2022-08-12 15:09:10 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='SciAugment',
+    version='0.2.0',
+    author='Martin Schätz',
+    author_email='martin.schatz.cz@gmail.com',
+    description='Package for augmentation of scientific images with YOLO annotations',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/martinschatz-cz/SciAugment',
+    project_urls = {
+        "Bug Tracker": "https://github.com/martinschatz-cz/SciAugment/issues"
+    },
+    license='BSD-3-Clause license',
+    packages=['SciSciAugment'],
+# list of packages that your package relies upon
+    install_requires=['cv2','albumentations','imgaug','copy','xml','sys','shutil'],
+)
