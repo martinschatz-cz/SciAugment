794e5d3054edbca37ba92d397197ef994adc0e3a
Martin Schätz <61615369+martinschatz-cz@users.noreply.github.com>
2024-02-11 15:20:27 +0100

    Create test_SciAug_tools.py - test
    first pytest functions to play with

---
+++ tests/test_SciAug_tools.py
@@ -0,0 +1,30 @@
+# test_SciAug_tools.py
+import os
+import SciAugment as SA
+from SciAugment.SciAug_tools import SciAugment
+
+def test_version():
+    version = SA.__version__
+    print("SciAugment version:", version)
+    assert isinstance(version, str), f"Expected result to be of type str, but got {type(result)}"
+
+def test_augment_data():
+    # Ensure that the example images are downloaded and extracted
+    #os.system("wget https://github.com/martinschatz-cz/SciAugment/raw/main/example_notebooks/subsection320.zip")
+    #os.system("unzip -q ./subsection320.zip -d ./")
+
+    # Define input parameters
+    input_images_folder = './' 
+    input_image_format = '.jpeg'
+
+    # Instantiate SciAugment and perform augmentation
+    aug1 = SciAugment()
+    aug1.augment_data(images_path=input_images_folder, image_format=input_image_format)
+
+    # Add assertions to check the outcome of the augmentation process
+    #assert os.path.exists("./train_data/"), "Augmented images folder does not exist"
+    #assert len(os.listdir("./train_data/")) > 0, "No augmented images generated"
+
+    # Cleanup: remove downloaded and extracted files
+    #os.remove("./subsection320.zip")
+    #os.system("rm -rf ./subsection320/")
