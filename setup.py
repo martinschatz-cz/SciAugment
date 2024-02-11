a3590e026e4698a9e813ed58398eb236e3a7554d
Martin Schätz <61615369+martinschatz-cz@users.noreply.github.com>
2024-02-11 14:06:59 +0100

    Update setup.py with package_data

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     },
     license='BSD-3-Clause license',
     packages=['SciAugment'],
+    package_data={'SciAugment': ['SciAug_tools.py']},  # Include SciAug_tools.py
     install_requires=[
         'git+https://github.com/albu/albumentations',
         'opencv-python-headless>=3.4.18.65,<4.2',  # specify the version range
