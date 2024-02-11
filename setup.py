afe18b450031cb5883577588f5e3f17b87d092a2
Martin Schätz <61615369+martinschatz-cz@users.noreply.github.com>
2024-02-11 14:45:50 +0100

    Update setup.py for git+albumentations

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     packages=['SciAugment'],
     package_data={'SciAugment': ['SciAugment/SciAug_tools.py']},  # Include SciAug_tools.py
     install_requires=[
-        'albumentations',
+        'git+https://github.com/albu/albumentations',
         'opencv-python-headless>=3.4.18.65,<4.2',
         'imgaug'
     ],
